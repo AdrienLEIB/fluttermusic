f27d74ad1b6fd493f1f39aeb3e604c74b00ed620
adrienleib <adrien.leib@ynov.com>
2021-10-28 12:59:45 +0200

    music is running

--- lib/Listen.dart
+++ lib/Listen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'Model/music.dart';
 
 
@@ -17,8 +19,15 @@ class Listen extends StatefulWidget{
 
 
 class ListenState extends State<Listen>{
-  double position= 0.0;
+  // double position= 0.0;
   bool isPlay = false;
+  statut lecture=statut.rewind;
+  // AudioPlayer audioPlayer = AudioPlayer();
+  Duration position = Duration(seconds: 0);
+  late StreamSubscription positionStream;
+  late StreamSubscription stateStream;
+  late AudioPlayer audioPlayer;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -55,12 +64,14 @@ class ListenState extends State<Listen>{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(Icons.fast_rewind),
+            // (lecture==statut.stopped)?Icon(Icons.play_arrow, size:40):Icon(Icons.stop_circle),
             IconButton(
               icon:(isPlay) ?   Icon(Icons.pause, size: 40) : Icon(Icons.play_arrow, size: 40),
               onPressed: (){
                 setState(()
                 {
                  isPlay = !isPlay;
+                 lecture == statut.playing ? pause() : lecture==statut.rewind ? play() : print('toto');
                 });
               },
             ),
@@ -68,20 +79,67 @@ class ListenState extends State<Listen>{
           ],
         ),
         Slider.adaptive(
-            value: position,
+            value: position.inSeconds.toDouble(),
+            min: 0.0,
+            max: widget.music.time,
             activeColor: Colors.blueAccent,
             inactiveColor: Colors.cyanAccent,
             onChanged: (va){
               setState(() {
-                position = va;
+                Duration time = Duration(seconds: va.toInt());
+                position = time;
               });
-              print(position);
-            })
+            }),
       ],
     );
   }
 
+  play() async{
+    configurationPlayer();
+    // await audioPlayer.stop();
+    int result = await audioPlayer.play(widget.music.path_song);
+    if (result == 1){
+      setState(() {
+        lecture = statut.playing;
+      });
+    };
+
+  }
+
+  pause() async{
+    int result = await audioPlayer.stop();
+    if (result == 1) {
+      setState(() {
+        lecture = statut.rewind;
+      });
+    };
+
+  }
+
+  configurationPlayer(){
+    audioPlayer = AudioPlayer();
+    audioPlayer.setVolume(1.0);
+    positionStream = audioPlayer.onAudioPositionChanged.listen((event) {
+      setState(() {
+        position = event;
+      });
+      stateStream = audioPlayer.onPlayerStateChanged.listen((event) {
+        setState(() {
+          if(event == statut.playing){
+
+          }
+        });
+      });
+    });
+  }
 }
 
+
+enum statut{
+  playing,
+  stopped,
+  rewind,
+  forward,
+}
 
 
